da66df26f4fdea42e15283ea4c8adb6585566ae6
ivArmelin <bruno.armelin9@gmail.com>
2022-11-19 18:42:11 -0300

    Adicionado a tela de cadastro

--- GreatHoliday/urls.py
+++ GreatHoliday/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('login', views.login),
     path('search', views.search, name='search'),
-    path('weather/<str:location>', views.requestWeather)
+    path('weather/<str:location>', views.requestWeather),
+    path('cadastrar', views.cadastrar, name='cadastrar'),
 ]
